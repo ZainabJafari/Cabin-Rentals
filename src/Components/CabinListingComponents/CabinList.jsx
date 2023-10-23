70aeb2a0c37e60e572627a78981b8c14c6398923
Zainab <zainabjafari998@gmail.com>
2023-10-23 16:38:07 +0200

    list page

---
+++ src/Components/CabinListingComponents/CabinList.jsx
@@ -0,0 +1,26 @@
+// import React, {useState} from 'react'
+// import axios from 'axios'
+
+// const CabinList = () => {
+
+//     const [getData, setGetData] = useState([])
+
+//     const loadData = async () => {
+//       const result = await axios.get("http://localhost:7777/api/cabin")
+//       setGetData(result.data)
+//     }
+//     // loadData()
+
+//   return (
+//     <div>
+//       {getData.map((cabin) => (
+//         <div>
+//           {/* <p>{cabin.cabinName}</p> */}
+//         </div>
+        
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default CabinList
--- src/Pages/CabinListing.jsx
+++ src/Pages/CabinListing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import CabinList from '../Components/CabinListingComponents/CabinList'
 const CabinListing = () => {
   return (
     <div>
         <Navbar />
-        
+        <CabinList />
+
     </div>
   )
 }
